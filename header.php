b8ef2b3a150e143c1eac0f73e61570e30afe6489
Matthis <matthis.amrt@gmail.com>
2021-08-17 15:30:50 -0400

    test animation resto 1/ spinner créer
    Attention Voir modification sur resto 1

--- header.php
+++
@@ -1,5 +0,0 @@
- <!--LOGO-->
- <a href="index.html"><i class="fas fa-arrow-left fleche"></i></a>
-        <header class="logo">
-            <img class="logo__img" src="Maquettes_Ohmyfood/images/logo/ohmyfood.png" alt="logo"/>
-        </header>
